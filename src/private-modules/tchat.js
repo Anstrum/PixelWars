498e813793416b72ab1596cf7f15c820e32c2511
Anstrum <nanaka.kumiho@gmail.com>
2022-10-04 23:30:23 +0200

    switching computer

--- src/private-modules/tchat.js
+++ src/private-modules/tchat.js
@@ -1,10 +1,74 @@
-//#region Public funcs
+import {WebSocketServer} from "ws";
 
-export const Init = (port) => {
-    
+
+//#region Public funcs
+let server = {
+    port: NaN,
+    sockets: [NaN],
 }
 
-//#endregion
+
+export const Init = (_port) => {
+    server.port = _port;
+    server.ws = new WebSocketServer({port:_port});
+}
 
 
-let Servers = []
+export const Close = () => {
+    server.port = null
+    sockets = []
+}
+
+
+
+
+/*
+SERVER
+import { WebSocketServer } from "ws";
+
+const server = new WebSocketServer({ port: 3000 });
+
+server.on("connection", (socket) => {
+  // send a message to the client
+  socket.send(JSON.stringify({
+    type: "hello from server",
+    content: [ 1, "2" ]
+  }));
+
+  // receive a message from the client
+  socket.on("message", (data) => {
+    const packet = JSON.parse(data);
+
+    switch (packet.type) {
+      case "hello from client":
+        // ...
+        break;
+    }
+  });
+});
+*/
+
+
+/*
+CLIENT
+const socket = new WebSocket("ws://localhost:3000");
+
+socket.addEventListener("open", () => {
+  // send a message to the server
+  socket.send(JSON.stringify({
+    type: "hello from client",
+    content: [ 3, "4" ]
+  }));
+});
+
+// receive a message from the server
+socket.addEventListener("message", ({ data }) => {
+  const packet = JSON.parse(data);
+
+  switch (packet.type) {
+    case "hello from server":
+      // ...
+      break;
+  }
+});
+*/
